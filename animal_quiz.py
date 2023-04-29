27d5d574ddf78fa6f25271d0e44b8108bd0a13fd
Blessing Ilesanmi <blessingilesanmi21@gmail.com>
2023-04-29 12:45:27 +1200

    animal quiz

--- animal_quiz.py
+++ animal_quiz.py
@@ -1,12 +1,23 @@
 score = 0
+lives = 3
 
-def checkGuess(question, answer):
+def checkAnswer(question, answer):
     if question == answer:
         print("Correct!")
         score += 1
     else:
+        if lives != 0 & lives <= 3:
+            print(f"You have {lives} left")
+            print("Enter 'yes' to use it or 'q' to quit")
+            response = input("> ")
+            if 
         
-
+    
 print("Guess the animal")
+print("\nQuiz instructions:")
+print("You have 3 lives through out the game")
+print("Lives are accessible when you miss the question")
+print("You can buy more lives to keep the quiz going")
+print("Quiz ends when you miss the question and don't have any live to sustain the quiz")
 
 answer = input("What is the slowest animal on land?: ")
