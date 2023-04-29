eab1b40e2c47e1f65291ef98ec1411af9058c5f9
Blessing Ilesanmi <blessingilesanmi21@gmail.com>
2023-04-29 12:34:46 +1200

    animal quiz

--- animal_quiz.py
+++ animal_quiz.py
@@ -1,4 +1,12 @@
 score = 0
 
+def checkGuess(question, answer):
+    if question == answer:
+        print("Correct!")
+        score += 1
+    else:
+        
+
 print("Guess the animal")
 
+answer = input("What is the slowest animal on land?: ")
