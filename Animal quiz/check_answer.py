9a34b00f2f94a89020665fd9ede66e34466302d3
Blessing Ilesanmi <blessingilesanmi21@gmail.com>
2023-04-29 14:16:53 +1200

    animal quiz

--- Animal quiz/check_answer.py
+++ Animal quiz/check_answer.py
@@ -15,8 +15,10 @@ def checkAnswer(guess, answer):
             print("\nOops, that was a wrong guess")
             if lives != 0 & lives <= 3:
                 response = input("Try again: ")
+                lives -= 1
                 if response != "q":
-                    lives -= 1
+                    exit(0)
             else:
-                print("You have no lives again. Game over!")
+                print("\nYou have no lives again")
+                print(f"Your score is {score}. Game over!")
                 exit(0)
