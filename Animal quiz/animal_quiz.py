308a24dd96f3e4f3a5b58601d3375ad2bd271fb7
Blessing Ilesanmi <blessingilesanmi21@gmail.com>
2023-04-29 13:16:32 +1200

    animal quiz

--- Animal quiz/animal_quiz.py
+++ Animal quiz/animal_quiz.py
@@ -1,11 +1,12 @@
+from check_answer import checkAnswer
 
-        
-    
 print("Guess the animal")
 print("\nQuiz instructions:")
 print("You have 3 lives through out the game")
 print("Lives are accessible when you miss the question")
 print("You can buy more lives to keep the quiz going")
 print("Quiz ends when you miss the question and don't have any live to sustain the quiz")
+print("Goodluck!")
 
-answer = input("What is the slowest animal on land?: ")
+question = "\nWhat is the slowest animal on land?: "
+answer = input(question)
