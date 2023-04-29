0cd0dc06aba3fb2c7d30733b1f2bcf0a3a404d84
Blessing Ilesanmi <blessingilesanmi21@gmail.com>
2023-04-29 13:11:15 +1200

    animal quiz

---
+++ Animal quiz/animal_quiz.py
@@ -0,0 +1,11 @@
+
+        
+    
+print("Guess the animal")
+print("\nQuiz instructions:")
+print("You have 3 lives through out the game")
+print("Lives are accessible when you miss the question")
+print("You can buy more lives to keep the quiz going")
+print("Quiz ends when you miss the question and don't have any live to sustain the quiz")
+
+answer = input("What is the slowest animal on land?: ")
--- Animal quiz/check_answer.py
+++ Animal quiz/check_answer.py
@@ -18,13 +18,3 @@ def checkAnswer(question, answer):
                 print("Now you can try again")
                 print(question)
             
-        
-    
-print("Guess the animal")
-print("\nQuiz instructions:")
-print("You have 3 lives through out the game")
-print("Lives are accessible when you miss the question")
-print("You can buy more lives to keep the quiz going")
-print("Quiz ends when you miss the question and don't have any live to sustain the quiz")
-
-answer = input("What is the slowest animal on land?: ")
