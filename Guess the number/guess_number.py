5639b67cec20468923a2f210a4cc99b31d53575d
Blessing Ilesanmi <blessingilesanmi21@gmail.com>
2023-04-30 21:09:26 +1200

    guess the number

--- Guess the number/guess_number.py
+++ Guess the number/guess_number.py
@@ -1,26 +1,45 @@
-from sys import exit
-from random import choice
-from guess_func import getSecretNumber, guessNumber
+from random import shuffle
 
-print("Guess the number game")
-print("I am thinking of a 3-digit number. Try to guess what it is")
-print("\nGame instructions:")
-print("Love means One digit is correct but in the wrong position")
-print("Peace means One digit is correct and in the right position")
-print("Joy means No digit is correct")
-print("You have 10 guesses")
-
-def guessGame():
+def gameIntro():
+    print("Guess the number game")
+    print("I am thinking of a 3-digit number. Try to guess what it is")
+    print("\nGame instructions:")
+    print("Love means One digit is correct but in the wrong position")
+    print("Peace means One digit is correct and in the right position")
+    print("Joy means No digit is correct")
+    print("You have 10 guesses")
+    
     response = input("\nAre you ready? Y/n: ")
     if response.lower() == "y":
-        print("I have thought of a number")
+        print("\nI have thought of a number")
         print("You have 10 guesses to get it")
-        the_number = getSecretNumber()
-        print(the_number)
-        guessNumber()
-    else:
-        exit(0)
-        
-guessGame()
 
+def getSecretNumber():
+    max_number = 3
+    numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+    shuffle(numbers)
+    secret_number = ""
+    for i in range(max_number):
+        secret_number += str(numbers[i])
+    return secret_number
+
+def guessNumber():
+    maxGuess = 10
+    numOfTrial = 1
+    guess = ""
+    secretNumber = getSecretNumber()
+    print(secretNumber)
+    while (maxGuess != 0 & maxGuess <= 10) & (guess != secretNumber):
+        guess = input(f"Guess #{numOfTrial}: ")
+        maxGuess -= 1
+        numOfTrial += 1
+        if secretNumber == guess:
+            print("Correct! You guess right")
+
+gameIntro()          
+guessNumber()     
+
+
+
+        
 
