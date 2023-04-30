99aaf012f7877e6978c2c94ad5dad72b8019c25b
Blessing Ilesanmi <blessingilesanmi21@gmail.com>
2023-04-30 17:40:51 +1200

    guess the number

---
+++ Guess the number/guess_number.py
@@ -0,0 +1,6 @@
+print("Guess the number")
+print("I am thinking of a 3-digit number. Try to guess what it is")
+print("\nGame instructions:")
+print("RightWrong means One digit is correct but in the wrong position")
+print("RightRight means One digit is correct and in the right position")
+print("WrongWrong means No digit is correct")
