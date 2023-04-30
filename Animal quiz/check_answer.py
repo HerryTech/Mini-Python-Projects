6adcfa6391b0c2d7a42682fcd316caf511aed0f0
Blessing Ilesanmi <blessingilesanmi21@gmail.com>
2023-04-30 16:31:03 +1200

    animal quiz

--- Animal quiz/check_answer.py
+++ Animal quiz/check_answer.py
@@ -13,15 +13,13 @@ def checkAnswer(guess, answer):
         print(f"Correct!. Your score is {score}")
     else:
         print("\nOops, that was a wrong guess!")
-        while (lives != 0 & lives <= 3):
-            guess = input("Try again: ")
-            if guess.lower() == answer.lower():
-                score += 1   
+        while True:
+            if lives != 0 & lives <=3:
+                guess = input("Try again: ")
                 lives -= 1
-                print(f"Correct!. Your score is {score}")
-            elif guess == "q":
-                exit(0)
+                print(lives)
+                if guess.lower() == answer.lower():
+                    score += 1
+                    print(f"Correct!. Your score is {score}")
             else:
-                print("\nYou have no lives again")
-                print(f"Your score is {score}. Game over!")
                 exit(0)
