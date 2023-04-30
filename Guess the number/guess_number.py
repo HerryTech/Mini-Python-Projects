da734ea8484fe0df3dacf7cf610c3d5135b89542
Blessing Ilesanmi <blessingilesanmi21@gmail.com>
2023-04-30 17:55:33 +1200

    guess the game

--- Guess the number/guess_number.py
+++ Guess the number/guess_number.py
@@ -1,6 +1,17 @@
-print("Guess the number")
+from sys import exit
+
+print("Guess the number game")
 print("I am thinking of a 3-digit number. Try to guess what it is")
 print("\nGame instructions:")
 print("RightWrong means One digit is correct but in the wrong position")
 print("RightRight means One digit is correct and in the right position")
 print("WrongWrong means No digit is correct")
+print("You have 10 guesses")
+
+response = input("\nAre you ready?Y/n: ")
+if response.lower() == "y":
+    print(2)
+else:
+    exit(0)
+
+
