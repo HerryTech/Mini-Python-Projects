b18220465b34c4b38bddb7bd75dba2ca182a3a25
Blessing Ilesanmi <blessingilesanmi21@gmail.com>
2023-04-30 18:46:12 +1200

    guess the number

--- Guess the number/guess_number.py
+++ Guess the number/guess_number.py
@@ -1,20 +1,19 @@
 from sys import exit
 from random import choice
+from guess_func import getSecretNumber
 
 print("Guess the number game")
 print("I am thinking of a 3-digit number. Try to guess what it is")
 print("\nGame instructions:")
-print("RightWrong means One digit is correct but in the wrong position")
-print("RightRight means One digit is correct and in the right position")
-print("WrongWrong means No digit is correct")
+print("Love means One digit is correct but in the wrong position")
+print("Peace means One digit is correct and in the right position")
+print("Joy means No digit is correct")
 print("You have 10 guesses")
 
 response = input("\nAre you ready? Y/n: ")
 if response.lower() == "y":
     print("I have thought of a number")
-    
-    the_number = f"{choice(numbers)}{choice(numbers)}{choice(numbers)}"
-    print(the_number)
+    the_number = getSecretNumber()
     guess = input("Guess the number?: ")
 else:
     exit(0)
