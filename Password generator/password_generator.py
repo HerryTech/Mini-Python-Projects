aba6c078c84121b76e72520b63adeec64a8af505
Blessing Ilesanmi <blessingilesanmi21@gmail.com>
2023-05-01 12:23:45 +1200

    password generator

--- Password generator/password_generator.py
+++ Password generator/password_generator.py
@@ -1,3 +1,7 @@
+from random import choice
+from string import punctuation
+from password_words import adjectives, nouns
+
 name = input("What should I call you?: ")
-print(f"\n{name}, Welcome to password generator")
+print(f"\n{name}, Welcome to password generator!")
 print("I will help you create strong and easy to remember password")
