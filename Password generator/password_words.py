afb9b18d6e3f4e3c7dd1fd080a7cd4af877377b6
Blessing Ilesanmi <blessingilesanmi21@gmail.com>
2023-05-01 14:30:42 +1200

    password generator

--- Password generator/password_words.py
+++ Password generator/password_words.py
@@ -1,3 +1,5 @@
 adjectives = ["brave", "alert", "good", "alive", "Blue", "angry", "black", "better", "busy", "calm", "clean", "clear", "cute", "dark", "eager", "easy", "fair", "famous", "fine", "gentle", "funny", "gentle", "gifted", "kind", "jolly", "lazy", "lively", "lonely", "nice", "odd", "plain", "real", "rich", "shiny", "shy", "super", "tasty", "long",]
 
 nouns = ["art", "way", "world", "map", "meat", "year", "music", "person", "data", "food", "law", "bird", "power", "love", "fact", "idea", "product", "area", "story", "media", "player", "week", "exam", "movie", "car", "dog", "book", "cup", "phone", "table", "house", "series", "sea", "army", "camera", "child", "goal", "news", "user", "night", "raod", "soup", "event", "office",]
+
+punctuations = ["@", "#", "$", "&", "_", "%", "=", "/", "?", "*", "+", "~"]
