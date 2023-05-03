0a3b2b5469e165a5cad686ddc948406288fe7533
Blessing Ilesanmi <blessingilesanmi21@gmail.com>
2023-05-03 15:11:25 +1200

    countdown calender

--- Countdown calender/countdown_calender.py
+++ Countdown calender/countdown_calender.py
@@ -1,7 +1,8 @@
-from tkinter import Tk, Canvas
+from tkinter import *
 from datetime import date, datetime
 
 window = Tk()
 canvas = Canvas(window, width = 800, height = 800, bg = "black")
 canvas.pack()
 canvas.create_text(100, 50, anchor = "w", fill = "orange", font= "Arial 28 bold underline", text ="Holidays countdown calender")
+window.mainloop()
