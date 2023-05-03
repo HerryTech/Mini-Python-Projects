10a28200e18f50f02dba54e4e023896d6f6eeca0
Blessing Ilesanmi <blessingilesanmi21@gmail.com>
2023-05-03 17:18:29 +1200

    countdown calender

--- Countdown calender/countdown_calender.py
+++ Countdown calender/countdown_calender.py
@@ -9,17 +9,28 @@ canvas.create_text(100, 50, anchor = "w", fill = "orange", font= "Arial 28 bold 
 
 def get_events():
     list_events = []
+    list = []
     filename = "Countdown calender\event.txt"
     with open(filename) as f:
         for line in f:
             line = line.strip("\n")
             current_event = line.split(",")
-            event_date = datetime.strptime(current_event[-1], "%d/%m/%y").date()
+            event_date = datetime.strptime(current_event[1], "%d/%m/%y").date()
+            current_event[1] = event_date
             list_events.append(current_event)
     return list_events
-    
-        
+      
 def days_between_dates(date1, date2):
     time_between = str(date1 - date2)
+    #number_of_days = time_between.split(" ")
+    return time_between
+    
+
     
 events = get_events()
+today = date.today()
+
+for event in events:
+    event_name = event[0]
+    days_until_event = days_between_dates(event[1], today)
+    print(days_until_event)
