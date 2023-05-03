e206e57f68110079a3653e28f907af7fce14ed1e
Blessing Ilesanmi <blessingilesanmi21@gmail.com>
2023-05-03 18:03:54 +1200

    countdown calender

--- Countdown calender/countdown_calender.py
+++ Countdown calender/countdown_calender.py
@@ -4,7 +4,7 @@ from datetime import date, datetime
 window = Tk()
 canvas = Canvas(window, width = 800, height = 800, bg = "black")
 canvas.pack()
-canvas.create_text(100, 50, anchor = "w", fill = "orange", font= "Arial 28 bold underline", text ="Holidays countdown calender")
+canvas.create_text(100, 50, anchor = "w", fill = "orange", font= "Courier 28 bold underline", text ="Holidays countdown calender")
 
 def get_events():
     list_events = []
