70a3052c1f60c751dfaf077fdcd9ced1884419f1
Blessing Ilesanmi <blessingilesanmi21@gmail.com>
2023-05-04 16:09:18 +0100

    capital

--- Country_capital/capital.py
+++ Country_capital/capital.py
@@ -2,22 +2,23 @@ from requests import get
 from tkinter import Tk, simpledialog, messagebox, Canvas
 import json
 
-query_country = simpledialog.askstring("Country", "Type the name of a country:")
-name = query_country
+while True:
+    query_country = simpledialog.askstring("Country", "Type the name of a country:")
+    name = query_country
 
-url = f"https://restcountries.com/v3.1/name/{name}"
-r = get(url)
-response = r.json()
-filename = "Country_capital/country.json"
-with open(filename, "w") as f:
-    json.dump(response, f, indent = 4)
+    url = f"https://restcountries.com/v3.1/name/{name}"
+    r = get(url)
+    response = r.json()
+    filename = "Country_capital/country.json"
+    with open(filename, "w") as f:
+        json.dump(response, f, indent = 4)
 
-with open(filename) as f:
-    country = json.load(f)
+    with open(filename) as f:
+        country = json.load(f)
 
-capital = country[0]["capital"][0]
+    capital = country[0]["capital"][0]
          
-messagebox.showinfo("Answer", f"The capital city of {name} is {capital}")
+    messagebox.showinfo("Answer", f"The capital city of {name} is {capital}")
 #window = Tk()
 
 
