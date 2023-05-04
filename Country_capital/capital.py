3cbbf2ca6fa9da6b29da0ee707d39e70889b79c6
Blessing Ilesanmi <blessingilesanmi21@gmail.com>
2023-05-04 16:18:45 +0100

    capital

--- Country_capital/capital.py
+++ Country_capital/capital.py
@@ -4,21 +4,24 @@ import json
 
 while True:
     query_country = simpledialog.askstring("Country", "Type the name of a country:")
-    name = query_country
+    name = query_country.capitalize()
 
     url = f"https://restcountries.com/v3.1/name/{name}"
     r = get(url)
-    response = r.json()
-    filename = "Country_capital/country.json"
-    with open(filename, "w") as f:
-        json.dump(response, f, indent = 4)
+    if r.status_code != 200:
+        messagebox.showerror("Error", "Not a valid country")
+    else:
+        response = r.json()
+        filename = "Country_capital/country.json"
+        with open(filename, "w") as f:
+            json.dump(response, f, indent = 4)
 
-    with open(filename) as f:
-        country = json.load(f)
+        with open(filename) as f:
+            country = json.load(f)
 
-    capital = country[0]["capital"][0]
+        capital = country[0]["capital"][0]
          
-    messagebox.showinfo("Answer", f"The capital city of {name} is {capital}")
+        messagebox.showinfo("Answer", f"The capital city of {name} is {capital}")
 #window = Tk()
 
 
