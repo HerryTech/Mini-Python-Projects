5b2ca7ba37b5b22d3d72736c95028f472365d3c0
Blessing Ilesanmi <blessingilesanmi21@gmail.com>
2023-05-04 18:11:01 +0100

    secret message

--- Secret message/secret_message.py
+++ Secret message/secret_message.py
@@ -1,1 +1,20 @@
 from tkinter import simpledialog, messagebox
+
+def get_task():
+    task = simpledialog.askstring("Task", "Do you want to encrypt or decrypt a message?")
+    return task
+
+def get_message():
+    messaage = simpledialog.askstring("Message", "Enter your secret message")
+    return messaage
+
+while True:
+    task = get_task()
+    if task == "encrypt":
+        message = get_message()
+        messagebox.showinfo("Message", f"Message to encrypt is: {message}")
+    elif task == "decrypt":
+        message = get_message()
+        messagebox.showinfo("Message", f"Message to decrypt is: {message}")
+    else:
+        break
