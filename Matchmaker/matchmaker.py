b55f2e0a50af8c3d26ee33beb63b8958054fbe3d
Blessing Ilesanmi <blessingilesanmi21@gmail.com>
2023-05-06 09:21:57 +0100

    match the symbols

--- Matchmaker/matchmaker.py
+++ Matchmaker/matchmaker.py
@@ -10,20 +10,21 @@ first_in_the_match = True
 previousX = 0
 previousY = 0
 button_symbols = {}
+
 symbols = [chr(9986), chr(128383), chr(9989), chr(128393), chr(8987), chr(128194), chr(9997), chr(9996), chr(128077), chr(128078), chr(128528), chr(9992), chr(10065), chr(10066)]
 
 shuffle(symbols)
 
-print(symbols)
+#print(symbols)
 
-for i in range(6):
-    for j in range(4):
+for i in range(2):
+    for j in range(2):
         button = Button(command = lambda i = i, j = j : show_symbol(i, j), width = 3, height = 3)
         button.grid(column = i, row = j)
         buttons[i, j] = button
-        button_symbols[i, j] = symbols.pop()
+        #button_symbols[i, j] = symbols.pop()
         
 def show_symbol():
     pass
 
-#root.mainloop()
+root.mainloop()
