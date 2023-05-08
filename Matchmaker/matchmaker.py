03427b0d22526ae9a93cbc867f100036c754e790
Blessing Ilesanmi <blessingilesanmi21@gmail.com>
2023-05-08 12:32:18 +0100

    match the symbols

--- Matchmaker/matchmaker.py
+++ Matchmaker/matchmaker.py
@@ -8,11 +8,9 @@ root.resizable(width = False, height = False)
 
 buttons = {}
 first_in_the_match = True
-previousX = 0
-previousY = 0
+previousI = 0
+previousJ = 0
 button_symbols = {}
-
-#symbols = [chr(9986), chr(128383), chr(9989), chr(128393), chr(8987), chr(128194), chr(9997), chr(9996), chr(128077), chr(128078), chr(128528), chr(9992), chr(10065), chr(10066)]
 
 symbols = [u'\u2702', u'\u2702', u'\u2705', u'\u2705', u'\u2708', u'\u2708', u'\u2709', u'\u2709', u'\u270A', u'\u270A', u'\u270B', u'\u270B', u'\u270C', u'\u270C', u'\u270F', u'\u270F', u'\u2712', u'\u2712',u'\u2714', u'\u2714', u'\u2716', u'\u2716', u'\u2728', u'\u2728']
 
@@ -29,22 +27,22 @@ for i in range(6):
         
 def show_symbol(i, j):
     global first_in_the_match
-    global previousX
-    global previousY
+    global previousI
+    global previousJ
     buttons[i, j]["text"] = button_symbols[i, j]
     buttons[i, j].update_idletasks()
     
     if first_in_the_match:
-        previousX = i
-        previousY = j
+        previousI = i
+        previousJ = j
         first_in_the_match = False
-    elif previousX != i or previousY != j:
-        if buttons[previousX, previousY]["text"] != buttons[i, j]["text"]:
+    elif previousI != i or previousJ != j:
+        if buttons[previousI, previousJ]["text"] != buttons[i, j]["text"]:
             time.sleep(0.5)
-            buttons[previousX, previousY]["text"] = ""
+            buttons[previousI, previousJ]["text"] = ""
             buttons[i, j]["text"] = ""
         else:
-            buttons[previousX, previousY]["command"] = DISABLED
+            buttons[previousI, previousJ]["command"] = DISABLED
             buttons[i, j]["command"] = DISABLED
         first_in_the_match = True
 root.mainloop()
